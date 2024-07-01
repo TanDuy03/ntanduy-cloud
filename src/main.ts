f276a329f89261a585d2ba7deb295153a19b2170
TanDuy03 <ng.tanduy261203@gmail.com>
2024-07-01 22:58:23 +0700

    chore: config firebase analytics

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import App from './App.vue'
 import Alpine from 'alpinejs'
 import { createHead } from '@vueuse/head'
 import VueWriter from "vue-writer";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const meta = createHead()
 createApp(App).use(VueWriter).use(meta).use(router).mount('#app')
@@ -17,3 +19,17 @@ Alpine.store('page', {
 })
 
 Alpine.start()
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVZdkhPAyFqYC0exPOlC2-pag28Z5cZxk",
+    authDomain: "ntanduy-2eb98.firebaseapp.com",
+    databaseURL: "https://ntanduy-2eb98-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "ntanduy-2eb98",
+    storageBucket: "ntanduy-2eb98.appspot.com",
+    messagingSenderId: "586088571005",
+    appId: "1:586088571005:web:5f0c209cbfe20d57bb9114",
+    measurementId: "G-9DDEDF74N2"
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
