8696c39569b5e7bfc4eba2b5dd96ff97d821118d
TanDuy03 <ng.tanduy261203@gmail.com>
2024-07-03 22:01:14 +0700

    feat: validate token cloudflare turnstile

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -47,7 +47,7 @@
             </div>
 
             <div class="mt-7 mb-3">
-                <button type="submit" @click="login" :disabled="token == null || token === undefined"
+                <button type="submit" @click="login" :disabled="token === '' || token === undefined"
                     class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 outline-none">
                     Sign in
                 </button>
