d98db38c1a2906f627ef5950b24a2d2de4710f65
TanDuy03 <ng.tanduy261203@gmail.com>
2024-07-04 21:14:37 +0700

    fix: clear module ts-node

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,5 +1,6 @@
 declare module 'alpinejs';
 declare module 'vue-writer';
+declare module 'express';
 declare module '*.vue' {
     import { DefineComponent } from 'vue';
     const component: DefineComponent<{}, {}, any>;
