b34dddeceb7eef8f94120981d5d439a3e60e346d
TanDuy03 <ng.tanduy261203@gmail.com>
2024-07-10 22:32:30 +0700

    chore: update config enviroment

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 getAnalytics(app)
 getPerformance(app)
-
-const authDomain = process.env.VITE_AUTH_DOMAIN
-console.log(authDomain)
