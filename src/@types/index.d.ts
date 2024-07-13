eb5cd2dddce16801706ffaa535ecda383870c4ef
TanDuy03 <ng.tanduy261203@gmail.com>
2024-07-13 13:28:29 +0700

    chore: add vercel speed insights

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,6 +1,7 @@
 declare module 'alpinejs';
 declare module 'vue-writer';
 declare module 'express';
+declare module '@vercel/speed-insights';
 declare module '*.vue' {
     import { DefineComponent } from 'vue';
     const component: DefineComponent<{}, {}, any>;
