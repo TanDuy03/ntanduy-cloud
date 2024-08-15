e4d9c60dda7c4fffcdef85b98901fa58ab5acf86
TanDuy03 <ng.tanduy261203@gmail.com>
2024-08-15 12:43:52 +0700

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import EnvironmentPlugin from "vite-plugin-environment"
+delete process.env['CommonProgramFiles(x86)'];
+delete process.env['ProgramFiles(x86)']
 
 export default defineConfig({
   plugins: [vue(), EnvironmentPlugin("all")],
